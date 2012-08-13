0171b14db239cd04be2829779f1f22db446f71f0
Nico Kaiser <nico@kaiser.me>
2012-08-14 00:25:14 +0200

    character

--- js/bandoneon.js
+++ js/bandoneon.js
@@ -194,7 +194,7 @@ Bandoneon.keys = [
 
 Bandoneon.modes = {
 
-  'major': [ 2, 2, 1, 2, 2, 2, 1 ],
+  'major': [ 2, 2, 1, 2, 2, 2, 1 ],
   'minor': [ 2, 1, 2, 2, 1, 2, 2 ],
   'chromatic': [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 
