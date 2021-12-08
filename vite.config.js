1fe08234474aebdbcb47e32cf933e359a2dc8cbe
Nico Kaiser <nico@kaiser.me>
2021-12-08 16:40:54 +0100

    chore: format vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import path from 'path';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 export default defineConfig({
-  plugins: [
-      vue(),
-      vueI18n({
-        include: path.resolve(__dirname, './src/i18n/**')
-      }),
-  ]
-})
+    plugins: [
+        vue(),
+        vueI18n({
+            include: path.resolve(__dirname, './src/i18n/**'),
+        }),
+    ],
+});
