270b6b5c6f4834b168dd26372deffbbd26ed8565
Nico Kaiser <nico@kaiser.me>
2022-11-30 19:40:09 +0100

    feat: legacy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import autoprefixer from 'autoprefixer';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
       include: path.resolve(__dirname, './src/locales/**'),
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
     }),
   ],
   css: {
