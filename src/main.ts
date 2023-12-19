d4591d85acf86d19424b3adbeaa18a6759be7fca
Nico Kaiser <nico@kaiser.me>
2023-12-19 18:27:40 +0100

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ const i18n = createI18n({
 });
 
 declare module 'pinia' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export interface DefineStoreOptionsBase<S, Store> {
     persist?: boolean;
   }
