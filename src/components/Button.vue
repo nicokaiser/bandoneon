36ecbdb0831c038a565b523a720ff94c5c21c595
Nico Kaiser <nico@kaiser.me>
2024-01-21 21:04:48 +0100

    feat: tweak switch

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,7 +2,7 @@
   <button
     :disabled="disabled"
     :class="{
-      'rounded-md border px-3 py-1.5': true,
+      'rounded-md border px-3 py-1.5 font-medium': true,
       'border-neutral-800 bg-neutral-800 text-white hover:bg-neutral-700 dark:border-neutral-200 dark:bg-neutral-100 dark:text-neutral-900 dark:hover:bg-neutral-200':
         active,
       'border-neutral-400 hover:bg-neutral-100 dark:border-neutral-600 dark:hover:bg-neutral-700':
--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -7,7 +7,7 @@
       :disabled="disabled"
     />
     <div
-      class="peer h-8 w-[60px] rounded-full border border-neutral-300 after:absolute after:start-[4px] after:top-[3px] after:h-[26px] after:w-[26px] after:rounded-full after:bg-neutral-800 after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 dark:after:bg-neutral-100"
+      class="peer h-8 w-[60px] rounded-full bg-neutral-800 after:absolute after:start-[4px] after:top-[3px] after:h-[26px] after:w-[26px] after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none peer-focus:ring-4 dark:bg-neutral-100 dark:after:bg-neutral-800"
     ></div>
   </div>
 </template>
