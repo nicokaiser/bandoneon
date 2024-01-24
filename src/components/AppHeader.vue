8baad4e151f537787dde2015b71865d7cc61e1c5
Nico Kaiser <nico@kaiser.me>
2024-01-24 18:40:05 +0100

    feat: implement useDark

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,11 +10,10 @@
     </RouterLink>
 
     <div class="inline-flex items-center gap-1">
-      <!--
-      <button class="p-2">
-        <QuestionMarkCircleIcon class="h-5 w-5" />
+      <button class="p-2" @click="isDark = !isDark">
+        <SunIcon v-if="!isDark" class="h-5 w-5" />
+        <MoonIcon v-else class="h-5 w-5" />
       </button>
-      -->
 
       <button type="button" class="p-2" title="Settings" @click="menu = !menu">
         <Bars3Icon class="h-5 w-5" />
@@ -28,12 +27,17 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import { useDark } from '../composables/useDark';
+
 import AppSettings from './AppSettings.vue';
 import Bars3Icon from './icons/Bars3Icon.vue';
-// import QuestionMarkCircleIcon from './icons/QuestionMarkCircleIcon.vue';
+import SunIcon from './icons/SunIcon.vue';
+import MoonIcon from './icons/MoonIcon.vue';
 
 const route = useRoute();
 const menu = ref(false);
+
+const { isDark } = useDark();
 
 watch(
   () => route.path,
---
+++ src/composables/useDark.ts
@@ -0,0 +1,67 @@
+import { computed, onScopeDispose, ref, watchEffect } from 'vue';
+
+export function useDark() {
+  let mediaQuery: MediaQueryList | undefined;
+  const preferredDark = ref(false);
+
+  const handler = (event: MediaQueryListEvent) => {
+    preferredDark.value = event.matches;
+  };
+
+  const cleanup = () => {
+    if (!mediaQuery) return;
+    mediaQuery.removeEventListener('change', handler);
+  };
+
+  const stopWatch = watchEffect(() => {
+    const isSupported =
+      window &&
+      'matchMedia' in window &&
+      typeof window.matchMedia === 'function';
+    if (!isSupported) return;
+    cleanup();
+    mediaQuery = window!.matchMedia('(prefers-color-scheme: dark)');
+    mediaQuery.addEventListener('change', handler);
+    preferredDark.value = mediaQuery.matches;
+  });
+
+  onScopeDispose(() => {
+    stopWatch();
+    cleanup();
+    mediaQuery = undefined;
+  });
+
+  const system = computed(() => (preferredDark.value ? 'dark' : 'light'));
+  const mode = ref(localStorage.getItem('color-scheme') || 'auto');
+
+  const isDark = computed<boolean>({
+    get() {
+      if (mode.value === 'auto') return system.value === 'dark';
+      return mode.value === 'dark';
+    },
+    set(v) {
+      const modeVal = v ? 'dark' : 'light';
+      if (system.value === modeVal) {
+        mode.value = 'auto';
+      } else {
+        mode.value = modeVal;
+      }
+      localStorage.setItem('color-scheme', mode.value);
+    },
+  });
+
+  watchEffect(() => {
+    const el = window?.document.querySelector('html');
+    if (!el) return;
+    const value = mode.value === 'auto' ? system.value : mode.value;
+    ['light', 'dark'].forEach((v) => {
+      if (value === v) {
+        el.classList.add(v);
+      } else {
+        el.classList.remove(v);
+      }
+    });
+  });
+
+  return { isDark, mode, system };
+}
