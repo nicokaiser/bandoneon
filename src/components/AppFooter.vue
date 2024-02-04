538cdcb56ab453b05860933605c7c89f7f2b30a0
Nico Kaiser <nico@kaiser.me>
2024-02-04 15:18:37 +0100

    chore: improve footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="my-10 text-center text-sm text-neutral-500 dark:text-neutral-400">
-    Made with <span class="text-red-500">♥</span> in Vienna. The source code is
-    available on
-    <a
-      class="underline"
-      target="_blank"
-      href="https://github.com/nicokaiser/bandoneon/"
-    >
-      GitHub</a
-    >.
+  <div
+    class="mx-auto my-10 flex flex-wrap justify-center gap-1 px-4 text-sm text-neutral-500 dark:text-neutral-400"
+  >
+    <div>Made with <span class="text-red-500">♥</span> in Vienna.</div>
+    <div>
+      The source code is available on
+      <a
+        class="underline"
+        target="_blank"
+        href="https://github.com/nicokaiser/bandoneon/"
+      >
+        GitHub</a
+      >.
+    </div>
   </div>
 </template>
