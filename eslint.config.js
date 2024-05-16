68af9f1ed8f8572d582837fd01b43649d435340d
Nico Kaiser <nico@kaiser.me>
2024-05-16 11:34:52 +0200

    chore: move to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginVue from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: true,
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
+  },
+  eslintConfigPrettier,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+];
