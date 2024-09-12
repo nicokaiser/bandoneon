3a4b6193b6b39b9f27434a6fd4bd1c1ffdf13f9d
nicokaiser <nicokaiser@users.noreply.github.com>
2024-09-12 09:01:48 +0000

    deploy: 016cdfb93ea8c2d0644ca9184eb0f4b856919f32

---
+++ assets/_...path_-legacy-DqjOv_dQ.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-YNboF503.js"],(function(e,t){"use strict";var n,r,c;return{setters:[e=>{n=e._,r=e.o,c=e.c}],execute:function(){const t={class:"mx-auto max-w-screen-md flex-1 p-16 text-center"};e("default",n({},[["render",function(e,n){return r(),c("div",t,"Not Found")}]]))}}}));
