8c0c8c5edd2e66216698fdc23ae56895ffffd1c8
Nico Kaiser <nico@kaiser.me>
2024-10-27 12:25:18 +0100

    feat: add test for chords

---
+++ test/chords.test.js
@@ -0,0 +1,25 @@
+import { Chord, ChordType, Note } from 'tonal';
+import { assert, describe, it } from 'vitest';
+import chords from '../src/data/chords';
+
+ChordType.add(['1P', '3M', '7m'], ['7']); // 7no5
+ChordType.add(['1P', '3m', '7d'], ['dim']); // dim7no5
+ChordType.add(['1P', '3m', '7m'], ['m7']); // m7no5
+ChordType.add(['1P', '3M', '7M'], ['M7']); // M7no5
+
+describe('chords', () => {
+  for (const direction in chords) {
+    describe(direction, () => {
+      for (const chord in chords[direction]) {
+        it(chord, () => {
+          // Compare chromas
+          assert.deepEqual(
+            chords[direction][chord].map(Note.chroma).toSorted(),
+            Chord.get(chord).notes.map(Note.chroma).toSorted(),
+            `Expected ${direction} ${chord} to be ${Chord.get(chord).notes}, but got ${chords[direction][chord]}`,
+          );
+        });
+      }
+    });
+  }
+});
